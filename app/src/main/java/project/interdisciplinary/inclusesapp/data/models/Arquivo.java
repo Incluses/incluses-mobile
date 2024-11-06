7d4c3d66ecab5b635e92a0469b77c664a12c56ad
Eduardo dos Anjos Lazaro de Toledo <eduardotoledo-ieg@instituto.ad>
2024-11-06 10:57:44 -0300

    add responsivity biograph for users

--- app/src/main/java/project/interdisciplinary/inclusesapp/data/models/Arquivo.java
+++ app/src/main/java/project/interdisciplinary/inclusesapp/data/models/Arquivo.java
@@ -7,12 +7,6 @@ public class Arquivo implements Serializable {
     private UUID id;
 
     private String nome;
-
-    private String s3Url;
-
-    private String s3Key;
-
-
     private String tamanho;
 
     private UUID fkTipoArquivoId;
@@ -21,8 +15,6 @@ public class Arquivo implements Serializable {
 
     public Arquivo(String nome, String tamanho, UUID fkTipoArquivoId) {
         this.nome = nome;
-        this.s3Url = "a";
-        this.s3Key = "a";
         this.tamanho = tamanho;
         this.fkTipoArquivoId = fkTipoArquivoId;
     }
@@ -37,21 +29,6 @@ public class Arquivo implements Serializable {
         this.nome = nome;
     }
 
-    public String getS3Url() {
-        return s3Url;
-    }
-
-    public void setS3Url(String s3Url) {
-        this.s3Url = s3Url;
-    }
-
-    public String getS3Key() {
-        return s3Key;
-    }
-
-    public void setS3Key(String s3Key) {
-        this.s3Key = s3Key;
-    }
 
     public String getTamanho() {
         return tamanho;
@@ -90,8 +67,6 @@ public class Arquivo implements Serializable {
         return "{" +
                 "\"id\": \"" + (id != null ? id.toString() : "null") + "\"," +
                 "\"nome\": \"" + nome + "\"," +
-                "\"s3Url\": \"" + s3Url + "\"," +
-                "\"s3Key\": \"" + s3Key + "\"," +
                 "\"tamanho\": \"" + tamanho + "\"," +
                 "\"fkTipoArquivoId\": \"" + (fkTipoArquivoId != null ? fkTipoArquivoId.toString() : "null") + "\"," +
                 "\"tipoArquivo\": " + (tipoArquivo != null ? tipoArquivo.toString() : "null") +
